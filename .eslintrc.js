ad6e7216bac8c37379c98dbb11b8f36ec25f6417
ShivamTaneja <shiivamtaneja@gmail.com>
2024-06-27 14:41:36 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "react/react-in-jsx-scope": "off",
+    "no-console": [
+      "warn",
+      {
+        allow: ["warn", "error"]
+      }
+    ],
+    "no-use-before-define": "error",
+    "no-unused-vars": "warn",
+    "react/prop-types": 0,
+    "@typescript-eslint/no-unused-vars": 0
+  }
+};
