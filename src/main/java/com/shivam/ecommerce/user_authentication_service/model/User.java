a577347d48905e9aad1820f00d14c3a0e1438d4d
ShivamTaneja <shiivamtaneja@gmail.com>
2024-06-30 22:21:00 +0530

    initial commit

---
+++ src/main/java/com/shivam/ecommerce/user_authentication_service/model/User.java
@@ -0,0 +1,23 @@
+package com.shivam.ecommerce.user_authentication_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String password;
+}
